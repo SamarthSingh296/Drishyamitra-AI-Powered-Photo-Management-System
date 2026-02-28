bb8673f67dfc3a8da207ad5cb49c5a6b6e46c5a5
Vashu Jain <mr.vashujain3020@gmail.com>
2026-02-28 10:46:49 +0530

    backend part 4

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect, useRef } from 'react';
+import React, { useState, useEffect } from 'react';
 import './App.css';
 
 let API_BASE = process.env.REACT_APP_API_URL || 'http://localhost:5000/api';
